de6d86af05d3299956f8a4cd846ea2921299e1f1
Karine Ferreira Mendes <karineferreiramendes@gmail.com>
2025-06-03 18:10:42 -0300

    junção do css e att do arquivo script.js

--- script.js
+++ script.js
@@ -1,3 +1,60 @@
+/*CADASTRO*/
+
+function validarEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  }
+
+  function validarNome(nome) {
+    return nome.trim().includes(" ");
+  }
+
+  function registerUser(event) {
+    event.preventDefault();
+    
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+
+    if (!validarNome(nome)) {
+      alert("Digite seu nome completo!");
+      return;
+    }
+
+    if (!validarEmail(email)) {
+      alert("Digite um e-mail válido!");
+      return;
+    }
+
+    if (senha.length < 6) {
+      alert("A senha deve ter pelo menos 6 caracteres.");
+      return;
+    }
+
+    localStorage.setItem(email, JSON.stringify({ nome, email, senha }));
+    window.location.href = "login.html";
+  }
+
+  /*LOGIN*/
+
+  function loginUser(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+
+    const user = JSON.parse(localStorage.getItem(email));
+
+    if (!user || user.senha !== senha) {
+      alert("Usuário ou senha inválidos.");
+      return;
+    }
+
+    window.location.href = "index.html";
+  }
+
+/*SEARCH*/
+
 const searchInput = document.getElementById('search-input');
 const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
